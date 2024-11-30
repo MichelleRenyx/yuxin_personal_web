63fe0321f11e84c20ca1922dcb564dfe00685cd5
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2024-11-30 11:58:30 +0800

    Experties

---
+++ src/components/Experties/Experties.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import css from "./Experties.module.scss";
+import { projectExperience, WhatDoIHelp } from "../../utils/data";
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer, textVariant } from "../../utils/motion";
+
+const Experties = () => {
+    return (
+        <motion.section 
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{once: false, amount: 0.25}}
+    
+        className={css.wrapper}>
+            <div className={`paddings yPaddings flexCenter innerWidth ${css.container}`}>
+                <div className={css.leftside}>
+                    {
+                        projectExperience.map((exp, i) => {
+                            return(
+                                <motion.div 
+                                variants={fadeIn("right", "tween", (i+1)*0.2, 1)}
+                                className={css.exp} key={i}>
+                                    <div className="flexcenter" style={{background: exp.bg}}>
+                                        <exp.icon size={25} color = "white"/>
+                                    </div>
+
+                                    <div>
+                                        <span>{exp.name}</span>
+                                        <span className="secondaryText">{exp.projects} Projects</span>
+                                    </div>
+                                </motion.div>
+                            )
+                        })
+                    }
+                </div>
+
+                <motion.div 
+                variants={textVariant(0.5)}
+                className={css.rightside}>
+                    <span className="primaryText">What I Excel At? </span>
+                    {
+                        WhatDoIHelp.map((paragraph, i) => {
+                            return <span className="secondaryText" key={i}>{paragraph}</span>
+                        })
+                    }
+
+                    <div className={`flexCenter ${css.stats}`}>
+                        <div className={`flexCenter ${css.stat}`}>
+                            <span className="primaryText">666+</span>
+                            <span className="secondaryText">第一个量化</span>
+                        </div>
+                        <div className={`flexCenter ${css.stat}`}>
+                            <span className="primaryText">888+</span>
+                            <span className="secondaryText">第二个量化</span>
+                        </div>
+                    </div> 
+                </motion.div>
+            
+            </div>
+        </motion.section>
+    )
+}
+
+export default Experties;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import css from "./Hero.module.scss";
 import {motion} from "framer-motion";
-import { staggerContainer } from "../../utils/motion";
+import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
 
 const Hero = () => {
     return (
@@ -15,35 +15,47 @@ const Hero = () => {
             className={`innerWidth ${css.container}`}>
 
                 <div className={css.upperElements}>
-                    <span className="primaryText">
+                    <motion.span 
+                    variants={fadeIn("right", "tween", 0.3, 1)}
+                    className="primaryText">
                         Hi, <br/> I'm Yuxin.
-                    </span>
-                    <span className="secondaryText">
-                    一句话简介
-                    </span>
+                    </motion.span>
+                    <motion.span 
+                    variants={fadeIn("left", "tween", 0.3, 1)} 
+                    className="secondaryText">
+                        一句话简介
+                    </motion.span>
                 </div>
 
-                <div className={css.person}>
-                    <img src="./person111.png" alt="" style={{ width: '612px', height: '410px' }} />
-                </div>
+                <motion.div 
+                variants={fadeIn("up", "tween", 0.3, 1)}
+                className={css.person}>
+                    <motion.img 
+                    variants={slideIn("up", "tween", 0.5, 1.3)}
+                    src="./person111.png" alt="" style={{ width: '612px', height: '410px' }} />
+                </motion.div>
                 
                 <a className={css.linkedIn} href="https://www.linkedin.com/in/yuxin-michelle-ren">
                     LinkedIn Page
                 </a>
 
                 <div className={css.lowerElements}>
-                    <div className={css.experience}>
+                    <motion.div 
+                    variants={fadeIn("right", "tween", 0.3, 1)}
+                    className={css.experience}>
                         <div className="primaryText">0</div>
                         <div className="secondaryText">
                             <div>Years</div>
                             <div>Experience</div>
                         </div>
-                    </div>
-                    <div className={css.certificate}>
+                    </motion.div>
+                    <motion.div 
+                    variants={fadeIn("left", "tween", 0.3, 1)}
+                    className={css.certificate}>
                         <img src="./certificate.png" alt="" />
                         <span>Graduate Student in Software Engineering</span>
                         <span>The University of Melbourne</span>
-                    </div>
+                    </motion.div>
                 </div>
             </motion.div>
         </section>
