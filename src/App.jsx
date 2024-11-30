81047821c6541d1e222e32192cd291bdc0936e71
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2024-11-30 21:39:13 +0800

    Works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Experties from "./components/Experties/Experties";
+import Works from "./components/Works/Works";
 import css from './styles/app.module.scss';
 const App = () => {
   return <div className={`bg-primary ${css.container}`}>
     <Header/>
     <Hero/>
     <Experties/>
+    <Works/>
   </div>
 };
 
