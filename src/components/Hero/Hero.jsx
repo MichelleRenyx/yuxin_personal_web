7eb11c5677dec0257c0d163337cc5c231d5d1dbd
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2026-01-21 00:12:53 +1100

    enhace: update hero photo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                 className={css.person}>
                     <motion.img 
                     variants={slideIn("up", "tween", 0.5, 1.3)}
-                    src="./yuxin_2.png" alt="" />
+                    src="./yuxin_5.png" alt="" />
                 </motion.div>
                 
                 
